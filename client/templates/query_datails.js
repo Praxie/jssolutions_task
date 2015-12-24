f12f559f08462cdd423aeeecdec3f3a07fd88ecb
Іван Камінський <ivan.kaminskyi@gmail.com>
2015-12-24 21:57:33 +0200

    edded export

--- client/templates/query_datails.js
+++ client/templates/query_datails.js
@@ -1,8 +1,24 @@
 Template.query_details.helpers({
-    venues: function() {
-        var id=Session.get("query_venues");
-       return id;
+    venues: function () {
+        var id = Session.get("query_venues");
+        return id;
     },
-    
-    
+
+    venuesNum: function () {
+        return Session.get("query_venues").length;
+    }
+
+});
+
+
+Template.query_details.events({
+    'click #download':function(event) {
+        var csv=Papa.unparse(Session.get("query_venues"), {
+            delimiter:";"
+        });
+        csv="sep=;\n"+csv;
+        var blob=new Blob([csv], {type: "text/csv;charset=utf-8"});
+        saveAs(blob, "export.csv");
+        console.log(csv);
+    }
 });
